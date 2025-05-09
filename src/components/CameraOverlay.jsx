15d54ad691d772a55d3d294bd7b2032c5f201ea3
Dmitrii <latyshev.me@yandex.ru>
2025-05-09 06:23:28 +0300

    Исправление камеры

--- src/components/CameraOverlay.jsx
+++ src/components/CameraOverlay.jsx
@@ -11,6 +11,9 @@ export default function CameraOverlay({ onClose, results = [] }) {
   const calibrationOffset = 0;
   const MIN_DISTANCE = 6;
 
+  const isTargeted = Math.abs(relativeAngle) < 2;
+
+
   useEffect(() => {
     const handleOrientation = (event) => {
       if (event.beta != null) {
