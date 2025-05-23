82b924097d69d47f1b9e3755412929fe880a2130
Dmitrii <latyshev.me@yandex.ru>
2025-05-23 20:19:43 +0300

    Правки

--- src/components/CameraOverlay.jsx
+++ src/components/CameraOverlay.jsx
@@ -13,7 +13,7 @@ export default function CameraOverlay({ onClose, results = [] }) {
   const fieldOfView = 60;
   const MIN_DISTANCE = 6;
   const smoothFactor = 0.1;
-  const angleAmplify = 1.5;
+  const angleAmplify = 5;
 
   const [showMarkers, setShowMarkers] = useState(true);
 
